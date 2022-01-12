3fbf8602d46fb3d136a1ef3bba493ae7754a65e0
MD. SABBIR AHMED <sabbir206314@gmail.com>
2022-01-12 19:05:52 +0600

    initial

---
+++ git_push.sh
@@ -0,0 +1,15 @@
+git add .
+
+echo 'Hello Sabbir'
+echo 'Welcome to Aroddho'
+echo 'Enter your commit message:'
+read commitMessage
+
+git commit -m "$commitMessage"
+
+git push 
+git push 
+
+echo 'Your Aroddho Successfully Updated'
+
+read
